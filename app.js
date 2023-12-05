40f3c50f11491e13a1ec343203b336a6c6d6a919
Kyle Padayachee <kyle44987@gmail.com>
2023-12-05 14:07:51 +0200

    fetch data - post request

---
+++ app.js
@@ -0,0 +1,34 @@
+const API_KEY = "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"
+
+// create a function to fetch API data (Post)
+
+// fetch data - post request
+async function fetchData() {
+  const response = await fetch("https://api.openai.com/v1/completions",{
+     method: "POST",
+     headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          "Content-Type": "application/json",
+
+
+     },
+
+     body: JSON.stringify({
+          model: "text-davinci-003",
+          prompt: "hello, how are you today?",
+          max_token: 7
+
+
+        })
+
+
+ 
+    })
+    
+   const data =  await response.json()
+   console.log(data)
+
+  
+}
+
+fetchData();
